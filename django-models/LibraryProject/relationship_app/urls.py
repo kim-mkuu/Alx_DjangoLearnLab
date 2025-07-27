89c8d0fc19d2fc313e4b372d59256e0297e6d60d
kim-mkuu <kimukuphilip10@gmail.com>
2025-07-27 20:04:27 +0300

    update to use django built-in form as well import the login function

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,15 +6,15 @@ from .views import list_books, LibraryDetailView
 urlpatterns = [
     # Home page
     path('', views.home, name='home'),
-    
+   
     # Function-based view URL pattern
     path('books/', views.list_books, name='list_books'),
-    
+   
     # Class-based view URL pattern
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-
-    # Authentication URLs patterns using Django's built-in views
+    
+    # Authentication URL patterns using Django's built-in views
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register_view, name='register'),
 ]
