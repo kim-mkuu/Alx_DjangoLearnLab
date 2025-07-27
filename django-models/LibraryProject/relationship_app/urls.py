79ee3102bbb87f87d95e3bb8a8ee853e8336a729
kim-mkuu <kimukuphilip10@gmail.com>
2025-07-28 02:05:17 +0300

    updated url paths

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
 
 
      # Permission-protected Book CRUD operations
-    path('books/add/', views.add_book, name='add_book'),
-    path('books/edit/<int:book_id>/', views.edit_book, name='edit_book'),
-    path('books/delete/<int:book_id>/', views.delete_book, name='delete_book'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:book_id>/', views.delete_book, name='delete_book'),
 ]
